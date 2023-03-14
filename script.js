212ba0a1509073f7e9379ca06624852be1e33f26
nrjxanl <shwnn06@naver.com>
2023-03-14 22:30:31 +0900

    edit files

---
+++ script.js
@@ -0,0 +1,43 @@
+function march() {
+    document.getElementById("mar").scrollIntoView();
+}
+function april() {
+    document.getElementById("apr").scrollIntoView();
+}
+function may() {
+    document.getElementById("may").scrollIntoView();
+}
+function june() {
+    document.getElementById("jun").scrollIntoView();
+}
+function july() {
+    document.getElementById("jul").scrollIntoView();
+}
+function august() {
+    document.getElementById("aug").scrollIntoView();
+}
+function september() {
+    document.getElementById("sep").scrollIntoView();
+}
+function october() {
+    document.getElementById("oct").scrollIntoView();
+}
+function november() {
+    document.getElementById("nov").scrollIntoView();
+}
+function december() {
+    document.getElementById("dec").scrollIntoView();
+}
+function january() {
+    document.getElementById("jan").scrollIntoView();
+}
+function february() {
+    document.getElementById("feb").scrollIntoView();
+}
+
+function mar3() {
+    document.getElementById("mar3").scrollIntoView({behavior: "smooth"});
+}
+function mar6() {
+    document.getElementById("mar6").scrollIntoView({behavior: "smooth"});
+}
