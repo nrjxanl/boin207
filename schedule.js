6402057d28b57b034c654c659fe7fb8b7b3d8d4a
nrjxanl <shwnn06@naver.com>
2023-09-02 01:47:42 +0900

    edit files

--- schedule.js
+++ schedule.js
@@ -1,12 +1,14 @@
 schedule = {
-    "date": ["0907", "0911"],
-    "schedule": ["사회문제탐구 도서 준비<br>문학 도서 준비", "지구과학 형성평가"],
-}
+    "date": ["d0303", "d0306", "d0313", "d0314", "d0331", "d0407", "d0412", "d0413", "d0417", "d0508", "d0516", "d0518", "d0521", "d0522", "d0523", "d0531", "d0607", "d0611", "d0612", "d0613", "d0615", "d0618", "d0619", "d0621", "d0824", "d0830", "d0831", "d0907", "d0911", "d0912"],
+    "schedule": ["자기주도학습 불참 확인서 제출", "물리학 I, 지구과학 I 수능특강 교재 구매<br>언어와 매체 프린트 파일 준비<br>건강 실태 조사서 제출<br>수학여행 참가 신청서 제출", "사회 문제 탐구 보고서 주제 선정<br>미술 칼, 도안, A3 켄트지 준비", "화학 발표", "자기주도학습 불참 확인서 제출", "지구과학 I H-R도 그리기 수행평가<br>여권 제출", "화학 실험 수행평가", "일본어 I 단어 수행평가", "사회 문제 탐구 논문 요약", "기하 형성평가", "영어 I 나만의 광고 제작 수행평가", "영어 I 말하기 수행평가<br>물리학 I 실험 수행평가", "영어 I 말하기 수행평가 대본 제출", "사회 문제 탐구 도서 준비", "언어와 매체 도서 준비<br>화학 수행평가", "일본어 I 여행지 비교 수행평가<br>영어 I 강의 후 설문지 제출", "수학 I 독후감 제출", "미술 PPT 제출", "사회 문제 탐구 서평 작성 수행평가", "언어와 매체 말하기 수행평가<br>문학 말하기 수행평가", "지구과학 I 수행평가", "물리학 I 주제 탐구 보고서 제출", "사회 문제 탐구 최종 보고서 작성 수행평가", "스포츠 생활 배구 수행평가", "자기주도학습 불참 확인서 제출", "화학 I 물 전기 분해 수행평가", "사회 문제 탐구 논문 주제 선정", "사회 문제 탐구 도서 준비<br>문학 도서 준비", "지구과학 I 형성평가", "음악 소금 준비"],
+    }    
 
 timetable = {
-    "mon": ["지구과학I<span>전아림</span>", "화학I<span>이은주</span>", "수학II<span>김성*</span>", "사회문제탐구<span>서정민</span>", "물리학I<span>장종목</span>", "영어II<span>백기곤</span>", "일본어I<span>양혜리</span>"],
-    "tue": ["독서<span>김용진</span>", "지구과학I<span>김태식</span>", "언어와매체<span>강이욱</span>", "물리학I<span>장종목</span>", "영어II<span>백기곤</span>", "기하<span>김보성</span>", "음악<span>한송이</span>"],
-    "wed": ["영어II<span>백기곤</span>", "수학II<span>조재희</span>", "언어와매체<span>이하은</span>", "사회문제탐구<span>서정민</span>", "물리학I<span>장종목</span>", "스포츠생활<span>이다영</span>", "화학I<span>김현화</span>"],
-    "thu": ["기하<span>김보성</span>", "화학I<span>이은주</span>", "진로<span>백기곤</span>", "수학II<span>김성*</span>", "독서<span>류신형</span>", "사회문제탐구<span>양승범</span>", "지구과학I<span>전아림</span>"],
-    "fri": ["독서<span>류신형</span>", "수학II<span>김성*</span>", "스포츠생활<span>이다영</span>", "일본어I<span>양혜리</span>", "창체<span></span>", "창체<span></span>", "-<span></span>"],
+    "sun": ["-<span></span>", "-<span></span>", "-<span></span>", "-<span></span>", "-<span></span>", "-<span></span>", "-<span></span>"],
+    "mon": ["지구과학 I<span>전아림</span>", "화학 I<span>이은주</span>", "수학 II<span>김성</span>", "사회 문제 탐구<span>서정민</span>", "물리학 I<span>장종목</span>", "영어 II<span>백기곤</span>", "일본어 I<span>양혜리</span>"],
+    "tue": ["독서<span>김용진</span>", "지구과학 I<span>김태식</span>", "언어와 매체<span>강이욱</span>", "물리학 I<span>장종목</span>", "영어 II<span>백기곤</span>", "기하<span>김보성</span>", "음악<span>한송이</span>"],
+    "wed": ["영어 II<span>백기곤</span>", "수학 II<span>조재희</span>", "언어와 매체<span>이하은</span>", "사회 문제 탐구<span>서정민</span>", "물리학 I<span>장종목</span>", "스포츠 생활<span>이다영</span>", "화학 I<span>김현화</span>"],
+    "thu": ["기하<span>김보성</span>", "화학 I<span>이은주</span>", "진로<span>백기곤</span>", "수학 II<span>김성</span>", "독서<span>류신형</span>", "사회 문제 탐구<span>양승범</span>", "지구과학 I<span>전아림</span>"],
+    "fri": ["독서<span>류신형</span>", "수학 II<span>김성</span>", "스포츠 생활<span>이다영</span>", "일본어 I<span>양혜리</span>", "창체<span></span>", "창체<span></span>", "-<span></span>"],
+    "sat": ["-<span></span>", "-<span></span>", "-<span></span>", "-<span></span>", "-<span></span>", "-<span></span>", "-<span></span>"],
 }
--- script.js
+++ script.js
@@ -9,8 +9,9 @@ if (month < 3) {
     $("table").eq(month + 10).css("display", "table");
 };
 
-// timetable array로 가져오기
+// json data array로 가져오기
 timetableList = Object.keys(timetable);
+scheduleList = Object.values(schedule.date);
 
 // 일별 dateClass 선언
 $("table").each(function () {
@@ -25,7 +26,7 @@ $("table").each(function () {
 
         dateClass = month + date;
 
-        $(this).attr("class", "d" + dateClass);
+        $(this).attr("class", "d" + dateClass).append("<p></p>");
 
         // 전월일 경우
         if ($(this).text().match("p")) {
@@ -40,7 +41,7 @@ $("table").each(function () {
                 dateClass = "0" + dateClass;
             };
 
-            $(this).attr("class", "d" + dateClass);
+            $(this).attr("class", "d" + dateClass).append("<p></p>");
         };
 
         // 익월일 경우
@@ -56,7 +57,7 @@ $("table").each(function () {
                 dateClass = "0" + dateClass;
             };
 
-            $(this).attr("class", "d" + dateClass);
+            $(this).attr("class", "d" + dateClass).append("<p></p>");
         };
 
         // 요일 등 class 지정 제외
@@ -66,23 +67,24 @@ $("table").each(function () {
 
         // 달력 클릭 시 일정, 시간표 표시
         $(this).click(function () {
+
             $("#bg").css("display", "block");
             $("#sched").css("display", "block");
 
-            schedDate = $(this).attr("class").replace(/[^0-9]/g, "");
+            schedDate = $(this).attr("class");
             scheduleList = Object.values(schedule.date);
             n = scheduleList.indexOf(schedDate);
 
             week = ["일", "월", "화", "수", "목", "금", "토"];
-            if (schedDate.substr(0, 2) == "01" || schedDate.substr(0, 2) == "02") {
-                date = "2024-" + schedDate.substr(0, 2) + "-" + $(this).attr("class").substr(3, 2);
+            if (schedDate.substr(1, 2) == "01" || schedDate.substr(1, 2) == "02") {
+                date = "2024-" + schedDate.substr(1, 2) + "-" + $(this).attr("class").substr(3, 2);
             } else {
-                date = "2023-" + schedDate.substr(0, 2) + "-" + $(this).attr("class").substr(3, 2);
+                date = "2023-" + schedDate.substr(1, 2) + "-" + $(this).attr("class").substr(3, 2);
             };
 
             week = week[new Date(date).getDay()];
 
-            $("#date").text(schedDate.substr(0, 2) + "." + schedDate.substr(2, 2) + ".(" + week + ")");
+            $("#date").text(Number(schedDate.substr(1, 2)) + "월 " + Number(schedDate.substr(3, 2)) + "일 " + week + "요일");
             $("#event").html(schedule.schedule[n]);
             $("#event:empty").text("일정이 없습니다.").css("opacity", ".5");
 
@@ -101,6 +103,11 @@ $("table").each(function () {
 
             $("#timetable > p").html(timetable1 + "<br>" + timetable2 + "<br>" + timetable3 + "<br>" + timetable4 + "<br>" + timetable5 + "<br>" + timetable6 + "<br>" + timetable7);
         
+            height = ($("#sched").innerHeight());
+            screenHeight = $(window).height();
+
+            $("#sched").css("top", (screenHeight - height) / 2);
+
         });
 
     });
@@ -110,7 +117,7 @@ $("table").each(function () {
 // 빈 공간 클릭 시 팝업 없애기
 $("#bg").click(function () {
     $("#bg").css("display", "none");
-    $("#sched").css("display", "none");
+    $("#sched").css({"display": "none", "top": "0"});
 
     $("#date").empty();
     $("#event").empty().css("opacity", "1");
@@ -121,7 +128,7 @@ $("#bg").click(function () {
 $(document).keydown(function (event) {
     if (event.keyCode == 27 || event.which == 27) {
         $("#bg").css("display", "none");
-        $("#sched").css("display", "none");
+        $("#sched").css({"display": "none", "top": "0"});
 
         $("#date").empty();
         $("#event").empty().css("opacity", "1");
@@ -138,9 +145,36 @@ if (date.length == 1) {
     date = "0" + date;
 };
 
-today = ".d" + month + date;
+today = "d" + month + date;
 
-$(today).css("border", "2px solid #c8c8c8");
+$("." + today).css("box-shadow", "0 0 0 2px #c8c8c8 inset");
+
+// 오늘의 일정 불러오기
+n = scheduleList.indexOf(today);
+
+week = ["일", "월", "화", "수", "목", "금", "토"];
+if (month == "01" || month == "02") {
+    date = "2024-" + month + "-" + date;
+} else {
+    date = "2023-" + month + "-" + date;
+};
+
+week = week[new Date(date).getDay()];
+
+$("#todaySchedule").append("<p>" + schedule.schedule[n] + "</p>");
+$("#todaySchedule > p:nth-of-type(1)").append("<span>" + Number(today.substr(1, 2)) + "월 " + Number(today.substr(3, 2)) + "일 " + week + "요일</span>");
+
+if($("#todaySchedule > p:nth-of-type(2)").text() == "undefined") {
+    $("#todaySchedule > p:nth-of-type(2)").empty().text("오늘 일정이 없습니다.").css("opacity", ".5");
+}
+
+// toToday 누르면 오늘 날짜로 이동
+$("#toToday > div").text(Number(today.substr(3, 2)))
+
+$("#toToday > div").click(function () {
+    $("table").css("display", "none");
+    $("table[class=" + today.substr(1, 2) + "]").css("display", "table");
+});
 
 // 전월 달력 불러오기
 $(".prevMonth").click(function () {
@@ -165,5 +199,23 @@ $(".nextMonth").click(function () {
 
     $("table").css("display", "none");
     $(this).parents("table").next().css("display", "table");
+
+});
+
+// 일별 일정 개수 표시
+$("td").each(function () {
+
+    date = $(this).attr("class");
+    scheduleList = Object.values(schedule.date);
+    n = scheduleList.indexOf(date);
+
+    if(n !== -1) {
+        count = schedule.schedule[n].replace(/[^<br>]/g, "").length / 4 + 1;
+        $(this).find("p").text(count);
+    };
+
+    if($(this).find("p").text().length !== 0) {
+        $(this).find("p").css("background", "#00f")
+    };
 
 });
